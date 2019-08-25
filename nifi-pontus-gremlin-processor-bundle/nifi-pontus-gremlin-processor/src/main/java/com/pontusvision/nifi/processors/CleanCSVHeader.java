1c7b98053dfc3984d5ffc29f6bdc0ee695e5f479
dev <dev@pontusvision.com>
2019-08-25 19:31:09 +0100

    added new features to CleanCSVHeader

--- nifi-pontus-gremlin-processor-bundle/nifi-pontus-gremlin-processor/src/main/java/com/pontusvision/nifi/processors/CleanCSVHeader.java
+++ nifi-pontus-gremlin-processor-bundle/nifi-pontus-gremlin-processor/src/main/java/com/pontusvision/nifi/processors/CleanCSVHeader.java
@@ -6,6 +6,7 @@
 package com.pontusvision.nifi.processors;
 
 import com.pontusvision.utils.StringReplacer;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.Tags;
 import org.apache.nifi.components.PropertyDescriptor;
@@ -27,14 +28,17 @@ import java.util.*;
  */
 @Tags({ "JSON", "CSV" }) @CapabilityDescription("Convert CSV to JSON.")
 
-public class CleanCSVHeader   extends AbstractProcessor
+public class CleanCSVHeader extends AbstractProcessor
 {
 
   private List<PropertyDescriptor> properties;
 
   private Set<Relationship> relationships;
-  String findText = null;
-  String replaceText = "_";
+  String  findText     = null;
+  String  replaceText  = "_";
+  String  prefixText   = "";
+  Boolean useRegex     = false;
+  String  csvDelimiter = ",";
 
   public static final String MATCH_ATTR = "match";
 
@@ -46,20 +50,35 @@ public class CleanCSVHeader   extends AbstractProcessor
       .name("Text to replace (in the first line)").defaultValue("_").required(true)
       .addValidator(StandardValidators.NON_EMPTY_VALIDATOR).build();
 
-  public static final Relationship SUCCESS = new Relationship.Builder().name("SUCCESS")
-      .description("Success relationship").build();
+  final static PropertyDescriptor USE_REGEX = new PropertyDescriptor.Builder()
+      .name("Use Regex").defaultValue("false").required(true)
+      .addValidator(StandardValidators.BOOLEAN_VALIDATOR).build();
 
-  public static final Relationship FAILURE = new Relationship.Builder().name("FAILURE")
-      .description("Failure relationship").build();
+  final static PropertyDescriptor CSV_REPLACEMENT_PREFIX = new PropertyDescriptor.Builder()
+      .name("Replacement Prefix").defaultValue("false").required(true)
+      .addValidator(StandardValidators.BOOLEAN_VALIDATOR).build();
+
+  final static PropertyDescriptor CSV_DELIMITER = new PropertyDescriptor.Builder()
+      .name("CSV Delimiter").defaultValue(",").required(true)
+      .addValidator(StandardValidators.BOOLEAN_VALIDATOR).build();
+
+  public static final Relationship SUCCESS = new Relationship.Builder().name("SUCCESS")
+                                                                       .description("Success relationship").build();
+
+  public static final Relationship FAILURE   = new Relationship.Builder().name("FAILURE")
+                                                                         .description("Failure relationship").build();
   public static final Relationship NEED_MORE = new Relationship.Builder().name("NEED_MORE").description(
       "Need More relationship, used if the header is being read and we need more data to process the next record")
-      .build();
+                                                                         .build();
 
   @Override public void init(final ProcessorInitializationContext context)
   {
     List<PropertyDescriptor> properties = new ArrayList<>();
     properties.add(CSV_FIND_TEXT);
     properties.add(CSV_REPLACE_TEXT);
+    properties.add(USE_REGEX);
+    properties.add(CSV_REPLACEMENT_PREFIX);
+    properties.add(CSV_DELIMITER);
 
     this.properties = Collections.unmodifiableList(properties);
 
@@ -70,13 +89,12 @@ public class CleanCSVHeader   extends AbstractProcessor
     this.relationships = Collections.unmodifiableSet(relationships);
   }
 
-
   private static final int BUFFER_SIZE = 2 * 1024 * 1024;
 
   private void copy(InputStream input, int offset, OutputStream output) throws IOException
   {
-     byte[] buffer = new byte[BUFFER_SIZE];
-    int bytesRead = input.read(buffer);
+    byte[] buffer    = new byte[BUFFER_SIZE];
+    int    bytesRead = input.read(buffer);
     while (bytesRead != -1)
     {
       output.write(buffer, 0, bytesRead);
@@ -98,14 +116,26 @@ public class CleanCSVHeader   extends AbstractProcessor
     {
       replaceText = newValue;
     }
+    else if (descriptor.equals(USE_REGEX))
+    {
+      useRegex = Boolean.parseBoolean(newValue);
+    }
+    else if (descriptor.equals(CSV_DELIMITER))
+    {
+      csvDelimiter = newValue;
+    }
+
+    else if (descriptor.equals(CSV_REPLACEMENT_PREFIX))
+    {
+      prefixText = newValue;
+    }
 
   }
 
   @Override public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException
   {
-    final ComponentLog log = this.getLogger();
-    final FlowFile flowfile = session.get();
-
+    final ComponentLog log      = this.getLogger();
+    final FlowFile     flowfile = session.get();
 
     session.read(flowfile, new InputStreamCallback()
     {
@@ -114,51 +144,72 @@ public class CleanCSVHeader   extends AbstractProcessor
         try
         {
 
-//          FlowFile ffile = flowfile;
-//          LineIterator li = IOUtils.lineIterator(in, Charset.defaultCharset().toString());
-//
-//          String header = li.nextLine();
+          //          FlowFile ffile = flowfile;
+          //          LineIterator li = IOUtils.lineIterator(in, Charset.defaultCharset().toString());
+          //
+          //          String header = li.nextLine();
 
-
-//          ffile = session.write(ffile, out -> out.write(headerBytes));
-
+          //          ffile = session.write(ffile, out -> out.write(headerBytes));
 
           FlowFile ffile = session.create();
           ffile = session.putAllAttributes(ffile, flowfile.getAttributes());
 
-
-          session.write(ffile,new OutputStreamCallback(){
+          session.write(ffile, new OutputStreamCallback()
+          {
             @Override public void process(OutputStream out) throws IOException
             {
 
               StringBuffer strbuf = new StringBuffer();
 
               byte val = -1;
-              do {
+              do
+              {
                 val = (byte) in.read();
                 if (val >= 0)
                 {
-                  strbuf.append((char)val);
+                  strbuf.append((char) val);
                 }
 
               } while (val != '\n' && val != -1);
 
-              String headerSub = StringReplacer.replaceAll(strbuf.toString(), (findText), replaceText);
+              String headerSub = null;
+              if (useRegex)
+              {
+                headerSub = strbuf.toString().replaceAll(findText, replaceText);
+              }
+              else
+              {
+                headerSub = StringReplacer.replaceAll(strbuf.toString(), (findText), replaceText);
+              }
+
+              if (StringUtils.isNotEmpty(prefixText))
+              {
+                String[]     headers = headerSub.split(csvDelimiter);
+                StringBuffer sb      = new StringBuffer();
+                for (int i = 0, ilen = headers.length; i < ilen; i++)
+                {
+                  if (i != 0)
+                  {
+                    sb.append(csvDelimiter);
+                  }
+                  sb.append(prefixText).append(headers[i].trim());
+                }
+                headerSub = sb.toString();
+
+              }
 
               final byte[] headerBytes = headerSub.getBytes();
 
               out.write(headerBytes);
-//              out.write("\n".getBytes());
-              copy(in,headerBytes.length + 1, out);
-//              in.close();
-//              out.close();
+              //              out.write("\n".getBytes());
+              copy(in, headerBytes.length + 1, out);
+              //              in.close();
+              //              out.close();
 
             }
           });
 
-
-          session.transfer(ffile,SUCCESS);
-
+          session.transfer(ffile, SUCCESS);
 
         }
         catch (Exception ex)
@@ -166,13 +217,13 @@ public class CleanCSVHeader   extends AbstractProcessor
         {
           ex.printStackTrace();
           log.error("Failed to read json string.");
-          session.transfer(flowfile,FAILURE);
+          session.transfer(flowfile, FAILURE);
         }
       }
     });
 
     session.remove(flowfile);
-//    session.commit();
+    //    session.commit();
 
   }
 
