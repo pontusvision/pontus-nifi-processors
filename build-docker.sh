a229cd6d05637f609b7410a4987be1d16e980e45
Leo Martins <lmartins@pontusvision.com>
2020-12-27 23:12:06 +0000

    updated nifi 1.12.1

--- build-docker.sh
+++ build-docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+set -e
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-cd $DIR/docker
-docker build --no-cache --rm . -t pontusvisiongdpr/pontus-nifi-gremlin-lib
+cd $DIR/
+TAG=${TAG:-latest}
+docker build --no-cache --rm . -t pontusvisiongdpr/pontus-nifi-gremlin-lib:${TAG}
 
-docker push pontusvisiongdpr/pontus-nifi-gremlin-lib
+docker push pontusvisiongdpr/pontus-nifi-gremlin-lib:${TAG}
 
